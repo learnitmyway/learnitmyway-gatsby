6d4b272af9cda58cf1a9028edf3097b5eab828be
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-06-20 21:22:56 +0200

    Move styles.cc to src/layouts

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Nav from '../components/Nav'
-import '../../static/css/styles.css'
+import './styles.css'
 
 class Template extends React.Component {
   render () {
