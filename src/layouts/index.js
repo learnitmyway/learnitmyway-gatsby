f5e68909fb2ada9978cd2ad87a3dfb590d93a8f9
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-06-20 21:04:49 +0200

    Remove starter header

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,55 +5,8 @@ import '../../static/css/styles.css'
 
 class Template extends React.Component {
   render () {
-    const { location, children } = this.props
-    console.log(this.props)
-    let header
+    const { children } = this.props
 
-    let rootPath = `/`
-    if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
-      rootPath = __PATH_PREFIX__ + `/`
-    }
-
-    if (location.pathname === rootPath) {
-      header = (
-        <h1
-          style={{
-            marginTop: 0
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: 'none',
-              textDecoration: 'none',
-              color: 'inherit'
-            }}
-            to={'/'}
-          >
-            Gatsby Starter Blog
-          </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3
-          style={{
-            fontFamily: 'Montserrat, sans-serif',
-            marginTop: 0
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: 'none',
-              textDecoration: 'none',
-              color: 'inherit'
-            }}
-            to={'/'}
-          >
-            Gatsby Starter Blog
-          </Link>
-        </h3>
-      )
-    }
     return (
       <div
         style={{
@@ -62,7 +15,6 @@ class Template extends React.Component {
         }}
       >
         <Nav />
-        {header}
         {children()}
       </div>
     )
