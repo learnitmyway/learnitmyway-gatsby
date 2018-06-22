0cd0ccdd146309b80db84b9136904290a0fbbb24
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-06-22 21:35:10 +0200

    Add popupCenter and apply it to ShareButtons

--- src/components/ShareButtons.js
+++ src/components/ShareButtons.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import Link from 'gatsby-link'
 
-const ShareButtons = ({pageUrl}) => {
+import popupCenter from './popupCenter'
+
+const ShareButtons = ({ pageUrl }) => {
+  function handleClick (url, e) {
+    popupCenter(url)
+    e.preventDefault()
+  }
+  const fbLink = `https://www.facebook.com/sharer/sharer.php?u=${pageUrl}`
   return (
     <div className='share-buttons-vertical nested-links'>
       <div className='share-buttons-vertical__text'>
         SHARE
       </div>
       <div>
-        <a className='popup' href={`https://www.facebook.com/sharer/sharer.php?u=${pageUrl}`}>
+        <a href={fbLink} onClick={(e) => handleClick(fbLink, e)}>
           <img height='32px' width='32px' src='/img/facebook.svg' />
         </a>
       </div>
---
+++ src/components/popupCenter.js
@@ -0,0 +1,44 @@
+// https://github.com/kni-labs/rrssb
+var popupCenter = function (url) {
+  var w = 580
+  var h = 470
+  // Fixes dual-screen position                         Most browsers      Firefox
+  var dualScreenLeft =
+    window.screenLeft !== undefined ? window.screenLeft : screen.left
+  var dualScreenTop =
+    window.screenTop !== undefined ? window.screenTop : screen.top
+
+  var width = window.innerWidth
+    ? window.innerWidth
+    : document.documentElement.clientWidth
+      ? document.documentElement.clientWidth
+      : screen.width
+  var height = window.innerHeight
+    ? window.innerHeight
+    : document.documentElement.clientHeight
+      ? document.documentElement.clientHeight
+      : screen.height
+
+  var left = width / 2 - w / 2 + dualScreenLeft
+  var top = height / 3 - h / 3 + dualScreenTop
+
+  var newWindow = window.open(
+    url,
+    'share-popup',
+    'scrollbars=yes, width=' +
+      w +
+      ', height=' +
+      h +
+      ', top=' +
+      top +
+      ', left=' +
+      left
+  )
+
+  // Puts focus on the newWindow
+  if (newWindow && newWindow.focus) {
+    newWindow.focus()
+  }
+}
+
+export default popupCenter
