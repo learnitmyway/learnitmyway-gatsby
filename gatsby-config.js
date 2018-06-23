1ea5fa43235bcab2bad6dc475300588eba5aa6c2
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-06-23 11:01:16 +0200

    Remove unwanted depencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,27 +23,18 @@ module.exports = {
               maxWidth: 590
             }
           },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`
-            }
-          },
           'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants'
+          'gatsby-remark-copy-linked-files'
         ]
       }
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // trackingId: `ADD YOUR TRACKING ID HERE`,
       }
     },
-    `gatsby-plugin-feed`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`
   ]
