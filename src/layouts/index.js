e91fb4dc922d0e672561913a6c8f9a635b2b7e97
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-06-25 19:56:11 +0200

    Import normalize.css first in layouts/index

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
+import 'normalize.css'
 import '../css/styles.css'
 import '../css/syntax.css'
-import 'normalize.css'
 
 class Template extends React.Component {
   render () {
