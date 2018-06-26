bf6d1573daccb151f9b25f3d0d32d41d74761151
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-06-26 21:49:43 +0200

    Add Nav to 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import Nav from '../components/Nav'
+
 const NotFoundPage = () => (
-  <div className='not-found nested-links'>
-    <h2>
-      <span>The requested page was not found. Return to </span>
-      <Link to='/'>home</Link>.
-    </h2>
-    <div className='not-found__gif' />
+  <div>
+    <Nav />
+    <div className='not-found nested-links'>
+      <h2>
+        <span>The requested page was not found. Return to </span>
+        <Link to='/'>home</Link>.
+      </h2>
+      <div className='not-found__gif' />
+    </div>
   </div>
 )
 
