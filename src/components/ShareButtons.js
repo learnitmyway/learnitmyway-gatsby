0e253a0aaa18bd74f1f8830d89038322d7dfe7a4
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-06-28 18:30:27 +0200

    Extract ShareButton component

--- src/components/ShareButtons.js
+++ src/components/ShareButtons.js
@@ -2,22 +2,28 @@ import React from 'react'
 
 import popupCenter from './popupCenter'
 
-const ShareButtons = ({ pageUrl }) => {
-  function handleClick (url, e) {
-    popupCenter(url)
+const ShareButton = ({ href, src }) => {
+  function handleClick (href, e) {
+    popupCenter(href)
     e.preventDefault()
   }
-  const fbLink = `https://www.facebook.com/sharer/sharer.php?u=${pageUrl}`
+
+  return (
+    <div>
+      <a href={href} onClick={(e) => handleClick(href, e)}>
+        <img height='32px' width='32px' src={src} />
+      </a>
+    </div>
+  )
+}
+
+const ShareButtons = ({ pageUrl }) => {
   return (
     <div className='share-buttons-vertical nested-links'>
       <div className='share-buttons-vertical__text'>
         SHARE
       </div>
-      <div>
-        <a href={fbLink} onClick={(e) => handleClick(fbLink, e)}>
-          <img height='32px' width='32px' src='/img/facebook.svg' />
-        </a>
-      </div>
+      <ShareButton href={`https://www.facebook.com/sharer/sharer.php?u=${pageUrl}`} src={'/img/facebook.svg'} />
     </div>
   )
 }
