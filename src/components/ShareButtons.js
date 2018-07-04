cc30194ca7fb1853450a42ce944964f6152f1a5b
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-07-04 19:50:26 +0200

    Rename props in ShareButtons to be more generic

--- src/components/ShareButtons.js
+++ src/components/ShareButtons.js
@@ -2,29 +2,29 @@ import React from 'react'
 
 import ShareButton from './ShareButton'
 
-export const FbShareButton = ({pageUrl}) => {
-  const fbHref = `https://www.facebook.com/sharer/sharer.php?u=${pageUrl}`
+export const FbShareButton = ({url}) => {
+  const fbHref = `https://www.facebook.com/sharer/sharer.php?u=${url}`
   return (
     <ShareButton href={fbHref} src={'/img/facebook.svg'} />
   )
 }
 
-export const TwitterShareButton = ({pageTitle, pageUrl}) => {
-  const twitterHref = `https://twitter.com/intent/tweet?text=${pageTitle}&via=DeveloperDavo%20${pageUrl}`
+export const TwitterShareButton = ({title, url}) => {
+  const twitterHref = `https://twitter.com/intent/tweet?text=${title}&via=DeveloperDavo%20${url}`
   return (
     <ShareButton href={twitterHref} src={'/img/twitter.svg'} />
   )
 }
 
-export const LinkedInShareButton = ({pageTitle, pageDescription, pageUrl}) => {
-  const linkedInHref = `http://www.linkedin.com/shareArticle?mini=true&url=${pageUrl}&title=${pageTitle}&summary=${pageDescription}`
+export const LinkedInShareButton = ({title, description, url}) => {
+  const linkedInHref = `http://www.linkedin.com/shareArticle?mini=true&url=${url}&title=${title}&summary=${description}`
   return (
     <ShareButton href={linkedInHref} src={'/img/linkedin.svg'} />
   )
 }
 
-export const EmailShareButton = ({pageTitle, pageDescription, pageUrl}) => {
-  const emailHref = `mailto:?subject=${pageTitle}&body=${pageDescription}%0A${pageUrl}`
+export const EmailShareButton = ({title, description, url}) => {
+  const emailHref = `mailto:?subject=${title}&body=${description}%0A${url}`
   return (
     <ShareButton href={emailHref} src={'/img/email.svg'} />
   )
